4530ae1cdbf21af8feeb532425ea9e1431cfaaef
eng6bear2wh5 <theanhnguyen16025@gmail.com>
2025-06-01 15:47:29 +0700

    fix bug medicinelist

--- Frontend/src/pages/PharmaInformation/MedicineList.jsx
+++ Frontend/src/pages/PharmaInformation/MedicineList.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllDrugs } from "../../api/drugApi";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "";
+const API_BASE = import.meta.env.VITE_REACT_APP_API_BASE || "";
 
 function MedicineList() {
   useEffect(() => {
