35f37beb179adf1ed4174a6c8a26c3d7c79d0492
eng6bear2wh5 <theanhnguyen16025@gmail.com>
2025-06-01 17:05:59 +0700

    Add function logout for user

--- Frontend/src/contexts/AuthContext.jsx
+++ Frontend/src/contexts/AuthContext.jsx
@@ -5,8 +5,25 @@ const AuthContext = createContext(null);
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null); // user là thông tin người dùng
 
+  const logout = async () => {
+    try {
+      await fetch('/auth/logout', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    } catch (e) {
+      console.error('Logout error: ', e);
+    }
+    setUser(null);
+    localStorage.removeItem('token');
+    navigate(`/`);
+  }
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, logout }}>
       {children}
     </AuthContext.Provider>
   );
